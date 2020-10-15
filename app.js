bf55c2d1811d1cf26bf946802e3c27da598ee1ff
gozolana <gozolana@gmail.com>
2020-10-16 06:20:18 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(new Date().toISOString(), '[/]');
+  res.send('Hello World');
+});
+
+app.listen(8080, () => {
+  console.log('Server Started');
+});
